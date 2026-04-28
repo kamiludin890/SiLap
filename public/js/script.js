78ba066881b00e6b9ccf6e5417388f8f1386fe90
Kamil Hidayat <kamilhidayat.890@gmail.com>
2026-04-28 15:57:07 +0700

    add readme

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,9 @@
 /** @format */
-
+$(document).ready(function () {
+  $.post("view/dashboard.php", function (data) {
+    $("#main-content").html(data);
+  });
+});
 const toggleBtn = document.getElementById("toggleSidebar");
 const sidebar = document.getElementById("sidebar");
 
