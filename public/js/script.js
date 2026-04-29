bcd2108bbd167bafe90e4805d9f3bf283284a25d
Kamil Hidayat <kamilhidayat.890@gmail.com>
2026-04-29 08:08:37 +0700

    perubahan style tombol sidenav ketika aktif

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,39 @@ $(document).ready(function () {
     $("#main-content").html(data);
   });
 });
-const toggleBtn = document.getElementById("toggleSidebar");
-const sidebar = document.getElementById("sidebar");
+$("#toggleSidebar").on("click", function () {
+  $("#sidebar").toggle();
+  $(this).find("i").toggleClass("bi-x-lg bi-list");
+});
+function setActive(el) {
+  $(".nav-link").removeClass("active"); // hapus semua
+  el.classList.add("active"); // aktifkan yang diklik
+}
 
-toggleBtn.addEventListener("click", function () {
-  sidebar.classList.toggle("d-none");
+$("#dashboard").click(function () {
+  $.post("view/dashboard.php", function (data) {
+    $("#main-content").html(data);
+  });
+  setActive(this);
+});
+
+$("#data").click(function () {
+  $.post("view/data.php", function (data) {
+    $("#main-content").html(data);
+  });
+  setActive(this);
+});
+
+$("#laporan").click(function () {
+  $.post("view/laporan.php", function (data) {
+    $("#main-content").html(data);
+  });
+  setActive(this);
+});
+
+$("#pengaturan").click(function () {
+  $.post("view/pengaturan.php", function (data) {
+    $("#main-content").html(data);
+  });
+  setActive(this);
 });
--- view/home.php
+++ view/home.php
@@ -7,25 +7,25 @@
         <ul class="nav flex-column">
 
             <li class="nav-item">
-                <label class="nav-link text-white active">
+                <label class="nav-link text-white active" id="dashboard">
                     <i class="bi bi-speedometer2"></i> Dashboard
                 </label>
             </li>
 
             <li class="nav-item">
-                <label class="nav-link text-white">
+                <label class="nav-link text-white" id="data">
                     <i class="bi bi-folder"></i> Data
                 </label>
             </li>
 
             <li class="nav-item">
-                <label class="nav-link text-white">
+                <label class="nav-link text-white" id="laporan">
                     <i class="bi bi-graph-up"></i> Laporan
                 </label>
             </li>
 
             <li class="nav-item">
-                <label class="nav-link text-white">
+                <label class="nav-link text-white" id="pengaturan">
                     <i class="bi bi-gear"></i> Pengaturan
                 </label>
             </li>
@@ -37,8 +37,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom sticky-top">
             <div class="container-fluid">
-                <button class="btn btn-primary" id="toggleSidebar">
-                    ☰
+                <button class="btn btn-primary" id="toggleSidebar"><i class="bi bi-x-lg"></i>
                 </button>
 
                 <label class="navbar-brand ms-2"><img src="" alt=""><i class="bi bi-person-fill"></i> User</label>
