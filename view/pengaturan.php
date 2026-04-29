cb5f4333692a0a264ad8cbdeea1dc0910c2fc266
Kamil Hidayat <kamilhidayat.890@gmail.com>
2026-04-29 14:45:13 +0700

    basic setting

--- view/pengaturan.php
+++ view/pengaturan.php
@@ -1,1 +1,47 @@
-penga
+<div id="second-content">
+    <div class="d-flex flex-column m-3 text-center">
+        <div class="row ">
+            <div class="col mt-2">
+                <div class="card card-hov">
+                    <div class="card-body">
+                        <i class="bi bi-person fs-1"></i>
+                        <h5 class="card-title">Pengguna</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-2">
+                <div class="card card-hov">
+                    <div class="card-body">
+                        <i class="bi bi-people fs-1"></i>
+                        <h5 class="card-title">Akun & Akses</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-2">
+                <div class="card card-hov">
+                    <div class="card-body" id="info-pengguna">
+                        <i class="bi bi-info-square fs-1"></i>
+                        <h5 class="card-title">Info Pengguna</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col mt-2">
+                <div class="card card-hov">
+                    <div class="card-body" id="database">
+                        <i class="bi bi-database fs-1"></i>
+                        <h5 class="card-title">Konfigurasi Database</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-2">
+                <div class="card card-hov">
+                    <div class="card-body" id="import_db">
+                        <i class="bi bi-database-gear fs-1"></i>
+                        <h5 class="card-title">Import Skema Database</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
